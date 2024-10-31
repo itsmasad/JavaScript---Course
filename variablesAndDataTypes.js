c88f532fd0156e13c3171747c1bfb6e92b435b81
itsmasad <m.asad@xpendless.com>
2024-10-31 08:13:31 +0500

    added let and var difference

--- variablesAndDataTypes.js
+++ variablesAndDataTypes.js
@@ -1,13 +1,35 @@
-// Defining the variable
-var x;
-// Assigning the value to the variable / Initialization
-x = 10;
-// Approach #2 | Doing both in single line | Declaretion + Intialization
-var z = 10;
+// // Defining the variable
+// var x;
+// // Assigning the value to the variable / Initialization
+// x = 10;
+// // Approach #2 | Doing both in single line | Declaretion + Intialization
+// var z = 10;
 
-// We can also assign multiple variables and their values in a single statement like this
-var a=100,b=200,c=300;
+// // We can also assign multiple variables and their values in a single statement like this
+// var a=100,b=200,c=300;
 
-document.write(x)
-console.log(x)
+// // let keyword is also used for varialbes, and its value can be change
+// let m = 1
+// m = 'Welcome'
+
+// document.write(m)
+// console.log(x)
+
+// Difference between var and let //
+// var is function scope
+// if the var is not defied will give you undefined
+console.log(x);
+var x = 10;
+console.log(x);
+
+// let is block scope
+// if the let is not defied will give you error
+// if will not be able to access let before initialization
+console.log(y);
+let y = 10;
+console.log(y);
+
+
+
+
 
