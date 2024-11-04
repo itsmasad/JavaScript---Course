2e8fcdd7e9e0080d3bd4306afaa1d75dc793e967
itsmasad <m.asad@xpendless.com>
2024-11-04 07:15:02 +0500

    Added arithmetic operators

---
+++ operators.js
@@ -0,0 +1,16 @@
+let x=2,y=6
+
+// Arithmetic Operators + - * / % **
+// These operators are used to perform methemetically calculations
+
+console.log(x+y); // 8
+console.log(x-y); // -4
+console.log(x*y); // 12
+console.log(y/x); // 3
+console.log(x%y); // 2
+console.log(x**y); // 64
+
+
+
+
+
