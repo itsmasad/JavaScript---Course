10478d7ba95ce1f4107127066da4a8be6f9e3f94
itsmasad <m.asad@xpendless.com>
2024-11-04 07:17:52 +0500

    added lines

--- operators.js
+++ operators.js
@@ -1,6 +1,6 @@
 let x=2,y=6
 
-// Arithmetic Operators + - * / % **
+// Arithmetic Operators + - * / % ** ++ --
 // These operators are used to perform methemetically calculations
 
 console.log(x+y); // 8
@@ -10,7 +10,8 @@ console.log(y/x); // 3
 console.log(x%y); // 2
 console.log(x**y); // 64
 
-
-
+x = x+1; // Approach 1 to add one to the value
+x++;     // Approach 2 to add one to the value
+console.log('Value of x:',x)
 
 
