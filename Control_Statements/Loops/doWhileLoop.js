efcb236fd4fa25ffdf3e64716f9d0ba292be29af
itsmasad <m.asad@xpendless.com>
2024-11-04 09:12:45 +0500

    Updated for loops

---
+++ Control_Statements/Loops/doWhileLoop.js
@@ -0,0 +1,17 @@
+// Do while loop
+// Do while loop will run the statement before checking the condition,
+// and will not run it again if the condition is not true
+
+// 1,2,3,4. . . .10
+let i = 30;
+do{
+console.log(i);
+i++;
+}while(i<=10);
+
+// 10,9,8,7. . . . 1
+let a = 10;
+do{
+console.log(a);
+a--;
+}while(a>0);
---
+++ Control_Statements/Loops/forLoop.js
@@ -0,0 +1,10 @@
+// For Loop
+/*****Syntex for For Loop*****/
+
+// for(initialization;condition;increment/decrement){
+// Statements;
+// }
+for(let i = 10;i>0;i--){
+    console.log(i)
+}
+
