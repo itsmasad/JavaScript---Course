a1ea5a43c04c8479eed160a0f8469a213fd349a8
itsmasad <m.asad@xpendless.com>
2024-11-04 08:37:35 +0500

    added while loop

---
+++ Control_Statements/Loops/whileLoop.js
@@ -0,0 +1,18 @@
+// While loop
+// While loop from 1 to 10
+let i = 1;
+
+while(i<=10){
+    console.log(i)
+    // We have to increment the value of i after every execution, so it can increase every time
+    // otherwise the loop will never end
+    i++
+}
+
+// While loop for even numbers (2,4,6,8)
+let a = 2;
+while(a<=10){
+    console.log(a)
+    // It will keep adding 2 in the current value of a
+    a+=2
+}
