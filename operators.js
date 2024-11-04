fe95cc9f772085c659528066e3dae880a64852d5
itsmasad <m.asad@xpendless.com>
2024-11-04 07:32:23 +0500

    updated

--- operators.js
+++ operators.js
@@ -10,8 +10,27 @@ console.log(y/x); // 3
 console.log(x%y); // 2
 console.log(x**y); // 64
 
-x = x+1; // Approach 1 to add one to the value
-x++;     // Approach 2 to add one to the value
-console.log('Value of x:',x)
+// x = x+1; // Approach 1 to post-increment
+// x++;     // Approach 2 to post-increment
+++x      // Pre-increment
+console.log('Value of x:',x) // 3
 
 
+// y = y-1; // Approach 1 to post-decrement
+y--; // Approach 1 to post-decrement
+--y; // Pre-decrement
+console.log('value of y',y)
+
+// Assignment Operators = 
+
+a = 100
+b = 50
+// a+=b;
+// console.log(a)
+console.log(a-=b) // a = a-b
+console.log(a*=b) // a = a*b
+console.log(a/=b) // a = a/b
+console.log(a%=b) // a = a%b
+
+// Relational / Comparison Operators
+// Always return a boolean value true/false
