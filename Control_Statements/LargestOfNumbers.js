cee03066ce1d913b6c6e2f5a84714459b743a7ea
itsmasad <m.asad@xpendless.com>
2024-11-04 08:11:27 +0500

    Updated conditions

--- Control_Statements/LargestOfNumbers.js
+++ Control_Statements/LargestOfNumbers.js
@@ -1,2 +1,14 @@
-let a=10,b=20,c=30;
+// Find the largest number
 
+let a=20,b=20,c=30;
+
+// If a is greater then b and a is greater than c, so its largest number
+if(a>b && a>c){
+    console.log(a," is the largest number")
+}else if(b>a && b>c)
+{
+    console.log(b," is the largest number")
+}else
+{
+    console.log(c," is the largest number")
+}
