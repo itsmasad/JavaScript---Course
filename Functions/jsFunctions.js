59c2c8f6f9c623ed75efc39605ceb853d4405bbc
itsmasad <m.asad@xpendless.com>
2024-11-04 10:02:44 +0500

    Updated code

---
+++ Functions/jsFunctions.js
@@ -0,0 +1,38 @@
+// Example 1
+
+// // Initializing the function
+// function add(a,b){
+//     console.log(a+b);
+// };
+
+// // Calling the function
+// add(100,200);
+
+// Example 2
+
+// function add(a,b){
+//     return(a+b);
+// }
+// let res = add(10,20)
+// console.log(res)
+
+// Example 3
+
+// // Without parameters
+// function greetings(){
+//     console.log("hey Good Morning");
+// }
+// // Calling the function
+// greetings();
+
+
+// Example 4
+
+// Without parameters / returning approach
+function greetings2(){
+    return("hey Good Morning");
+}
+// Calling the function
+let msg = greetings2();
+
+console.log(msg)
