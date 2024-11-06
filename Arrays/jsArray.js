eb25054202960bd20a83a0bb25583f508b4ae0e4
itsmasad <m.asad@xpendless.com>
2024-11-06 08:40:59 +0500

    Updated code

---
+++ Arrays/jsArray.js
@@ -0,0 +1,14 @@
+// JavaScript array itself is a predefined object
+
+// Approach 1
+let car1 = ["Honda","Suzuki","Toyota"];
+
+// Approach 2
+let car2 = new Array("Honda","Suzuki","Toyota");
+
+// Calling value from the array
+console.log(car2[0])
+
+// Changing the value
+car2[0] = "BMW"
+console.log(car2[0])
